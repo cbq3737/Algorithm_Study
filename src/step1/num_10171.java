96d1e354a15143ea660a0a1e6de78f4b76f62393
unknown <cbq3737@naver.com>
2021-03-17 17:40:54 +0900

    Algorithm Study

---
+++ src/step1/num_10171.java
@@ -0,0 +1,15 @@
+package step1;
+
+public class num_10171 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");	
+		
+		
+		
+		
+	}
+}
