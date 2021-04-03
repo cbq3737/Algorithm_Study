f0afbeb220cc01ab2fabceb92b82673a788704b6
unknown <cbq3737@naver.com>
2021-04-03 20:56:28 +0900

    함수

---
+++ src/step6/num_15596.java
@@ -0,0 +1,25 @@
+package step6;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class num_15596 {
+	public static long sum(int[] a) {
+		long ans = 0;
+		for(int i =0;i<a.length;i++) {
+			ans += a[i];
+		}
+		return ans;
+	}
+	public static void  main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		long hap = sum(arr);
+	}
+}
---
+++ src/step6/num_4673.java
@@ -0,0 +1,28 @@
+package step6;
+
+public class num_4673 {
+	
+	public static int self_Num(int num) {
+		int sum = num;
+		while(num!=0) {
+			sum = sum+(num%10);
+			num = num/10;
+		}
+		return sum;
+	}
+	
+	public static void main(String[] args) {
+		Boolean[] bl = new Boolean[10001];
+		for(int i=1;i<10001;i++) {
+			int num = self_Num(i);
+			if(num<10001) {				
+				bl[num]=true;
+			}
+		}
+		for(int i=1;i<10001;i++) {
+			if(bl[i]==null) {
+				System.out.println(i);	
+			}
+		}
+	}
+}
