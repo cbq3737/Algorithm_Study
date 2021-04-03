8a274bad2d0f0336da90e24428a29175a8e2bc26
unknown <cbq3737@naver.com>
2021-04-03 20:55:55 +0900

    1차 배열

---
+++ src/step5/num_4344.java
@@ -0,0 +1,40 @@
+package step5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class num_4344 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int cases = Integer.parseInt(br.readLine());
+		for(int i=0;i<cases;i++) {
+			StringTokenizer tk = new StringTokenizer(br.readLine());
+			int studentNum = Integer.parseInt(tk.nextToken());
+			int[] score = new int[studentNum];
+			double sum = 0;
+			double avg = 0;
+			int count = 0;
+			for(int j=0;j<studentNum;j++) {
+				score[j]= Integer.parseInt(tk.nextToken());
+				sum+= score[j];				
+			}
+			avg = sum/(studentNum);
+			for(int j=0;j<studentNum;j++) {
+				if(score[j]>avg) {
+					count+=1;
+				}		
+			}
+			double avgRatio = ((double)count/(studentNum))*100;
+			bw.write(String.format("%.3f", avgRatio)+"%");
+			bw.newLine();
+		}
+		br.close();
+		bw.close();
+	}
+}
---
+++ src/step5/num_8958.java
@@ -0,0 +1,48 @@
+package step5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class num_8958 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int num = Integer.parseInt(br.readLine());
+		String[] strs= new String[num];
+		for(int i=0;i<strs.length;i++) {
+			StringTokenizer tk = new StringTokenizer(br.readLine());
+			strs[i]= tk.nextToken();
+			int count = 0;
+			int score =0;
+			for(int j=0;j<strs[i].length();j++) {
+				if(strs[i].charAt(j)=='O'||strs[i].charAt(j)=='o') {
+					score +=1;
+//					if(j==0) {
+//						score = 1;
+//					}
+//					else if(strs[i].charAt(j-1)=='O'||strs[i].charAt(j-1)=='o') {
+//						score +=1;
+//					}
+//					else {
+//						score = 1;
+//					}
+				}
+				else {
+					score = 0;
+				}
+				count+=score;
+				
+			}
+			bw.write(String.valueOf(count));
+			bw.newLine();
+		}
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}
