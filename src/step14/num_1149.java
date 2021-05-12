0984d914b72635358c9f80995e210ef5aacdc0bb
unknown <cbq3737@naver.com>
2021-05-12 17:10:15 +0900

    DP

---
+++ src/step14/num_1149.java
@@ -0,0 +1,46 @@
+package step14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class num_1149 {
+	public static Integer[][] arr= new Integer[1001][3];
+	public static int[][] cost = new int[1001][3];
+	public static int paint_cost(int N,int color) {
+		if(cost[N][color] == 0 ) {
+			if(color ==0) {		//맨마지막 값
+				return cost[N][color] = Math.min(paint_cost(N-1,1),paint_cost(N-1,2))+arr[N][0]; 
+			}
+			else if(color ==1) {
+				return cost[N][color] = Math.min(paint_cost(N-1,0),paint_cost(N-1,2))+arr[N][1]; 
+			}
+			else if(color ==2) {
+				return cost[N][color] = Math.min(paint_cost(N-1,0),paint_cost(N-1,1))+arr[N][2]; 
+			}
+		}
+		return cost[N][color];
+	} 
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int nNum = Integer.parseInt(br.readLine());
+		for(int i=0;i<nNum;i++) {
+			StringTokenizer tk = new StringTokenizer(br.readLine());
+			int red_price = Integer.parseInt(tk.nextToken());
+			int green_price = Integer.parseInt(tk.nextToken());
+			int blue_price = Integer.parseInt(tk.nextToken());
+			arr[i][0] = red_price;
+			arr[i][1] = green_price;
+			arr[i][2] = blue_price;
+		}
+			cost[0][0] = arr[0][0];
+			cost[0][1] = arr[0][1];
+			cost[0][2] = arr[0][2];
+			
+			int result = Math.min(paint_cost(nNum-1,0),Math.min(paint_cost(nNum-1,1),paint_cost(nNum-1,2)));		
+			System.out.println(result);
+			
+	}
+}
---
+++ src/step14/num_9461.java
@@ -0,0 +1,33 @@
+package step14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class num_9461 {
+	public static long[] arr = new long[101];
+	public static long Fibo(int N) {
+		if(arr[N]== -1) {
+			return arr[N] = Fibo(N-2)+Fibo(N-3);
+		}
+		return arr[N];
+	}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb= new StringBuilder();
+		for(int i=0;i<101;i++) {
+			arr[i] = -1;
+		}
+		arr[0] = 0;
+		arr[1] = 1;
+		arr[2] = 1;
+		arr[3] = 1;
+		
+		for(int i=0;i<T;i++) {
+			int N = Integer.parseInt(br.readLine());
+			sb.append(Fibo(N)).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
