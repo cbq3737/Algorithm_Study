c150fb1cb1b0a0c3d86103256c9b56055f12ea4f
cbq3737 <33895624+cbq3737@users.noreply.github.com>
2021-08-11 01:04:50 +0900

    Update swea_1233.java

--- SWEA/D4/swea_1233.java
+++ SWEA/D4/swea_1233.java
@@ -1,16 +1,15 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class swea_1233 {
 	public static boolean flag;
-	public static int left;
-	public static int right;
-	public static char[] command = {'+','-','/','*'};
+	public static Queue<Integer> queue = new LinkedList<Integer>();
 	public static void dfs(String[] str,int idx,String element) {
 		
 		if(str[idx].equals("+")||str[idx].equals("-")||str[idx].equals("*")||str[idx].equals("/")) {
@@ -29,18 +28,41 @@ public class swea_1233 {
 		}
 		
 	}
+	public static void bfs(String[] str,int idx) {
+		queue.offer(1);
+		while(!queue.isEmpty()) {
+			int current = queue.poll();
+			
+			if(str[current].equals("-")||str[current].equals("-")||str[current].equals("*")||str[current].equals("/")) {
+				if(current*2>str.length-1 && current*2+1>str.length-1) {
+					flag= true;
+					queue.clear();
+					break;
+				}
+			}
+			if(current*2<=str.length-1) {
+				queue.offer(current*2);
+			}
+			if(current*2+1<=str.length-1) {
+				queue.offer(current*2+1);
+			}			
+		}
+	}
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		for(int t=0;t<10;t++) {
 			int num = Integer.parseInt(br.readLine());
 			String[] str = new String[num+1];
+			
 			while(num-->0) {
 				StringTokenizer tk = new StringTokenizer(br.readLine());
 				int idx = Integer.parseInt(tk.nextToken());
 				str[idx] = tk.nextToken(); //내가 갖고있는 것.
 			}
 			flag= false;
-			dfs(str,1,null);
+//			dfs(str,1,null);
+			
+			bfs(str,1);
 			if(!flag) {
 				System.out.println("#"+(t+1)+" "+1);
 			}else {
