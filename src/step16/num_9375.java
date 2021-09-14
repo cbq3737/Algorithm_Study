03f1b28e8402b9f0be56bd8450db7078cafa2303
unknown <cbq3737@naver.com>
2021-09-15 02:36:48 +0900

    math

--- src/step16/num_9375.java
+++ src/step16/num_9375.java
@@ -1,7 +1,43 @@
 package step16;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 public class num_9375 {
-	public static void main(String[] args) {
-		
+	public static int cnt;
+	public static String[] arr;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int nNum = Integer.parseInt(br.readLine());
+		for(int i=0;i<nNum;i++) {
+			Map<String,Integer> hmap = new HashMap<String, Integer>();
+			int tcase= Integer.parseInt(br.readLine());
+			arr = new String[tcase];
+			for(int j=0;j<tcase;j++) {
+				StringTokenizer tk = new StringTokenizer(br.readLine());
+				tk.nextToken();
+				String key = tk.nextToken();
+				if(hmap.containsKey(key)) {
+					hmap.put(key, hmap.get(key)+1);
+				}else {
+					hmap.put(key,1);
+				}
+			}
+			int sum =1;
+			List<String> list = new ArrayList<String>(hmap.keySet());
+			for(int j=0;j<list.size();j++) {
+				int value = hmap.get(list.get(j));
+				sum*= (value+1);
+			}
+			sb.append(sum-1+"\n");
+		}
+		System.out.println(sb);
 	}
 }
