3b15252e65ed7b6d5d66c504b467176eb0079760
unknown <cbq3737@naver.com>
2021-09-27 23:08:47 +0900

    정렬

--- README.md
+++ README.md
@@ -201,10 +201,12 @@
 - [미세먼지 안녕!](https://github.com/cbq3737/Algorithm_Study/blob/master/src/swea_test/num_17144.java)
 - [마인크래프트](https://github.com/cbq3737/Algorithm_Study/blob/master/src/swea_test/num_18111.java)
 - [동전](https://github.com/cbq3737/Algorithm_Study/blob/master/src/step14/num_9084.java)
+- [숫자놀이](https://github.com/cbq3737/Algorithm_Study/blob/master/src/step12/num_1755.java)
+
 ## ❌ 미해결 문제
 - [이항 계수2](https://www.acmicpc.net/problem/11051)	
 - [효율적인 해킹](https://github.com/cbq3737/Algorithm_Study/blob/master/src/step23/num_1325.java)
-
+- [백남이의 여행준비](https://github.com/cbq3737/Algorithm_Study/blob/master/src/step14/num_23061.java)
 
 # ✒️FastCampus	
 
---
+++ src/step12/num_1755.java
@@ -0,0 +1,88 @@
+package step12;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class num_1755 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer tk = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int[] alpha = new int[] { 9, 4, 8, 7, 2, 1, 6, 5, 0, 3 }; // 알파벳에 따른 우선순위
+		int mNum = Integer.parseInt(tk.nextToken());
+		int nNum = Integer.parseInt(tk.nextToken());
+		List<Integer> arr = new ArrayList<>();
+		List<Integer> list = new ArrayList<>();
+
+		for (int i = 0; i < nNum - mNum + 1; i++) { // 범위 값 배열에 넣어주기
+			arr.add(mNum + i);
+		}
+
+		int temp = 0;
+		int idx = 0;
+		// 1~99까지, 즉 10보다 크면 10의자리수 확인 후, 10의자리 동일하다면 1의 자리 판단
+		while (arr.size() != 0) { // 리스트 빌 때까지
+			int num = arr.get(0);
+			temp = num;
+			idx = 0;
+			for (int j = 0; j < arr.size(); j++) {
+				int num2 = arr.get(j);
+				int ten_num2 = 0;
+				int one_num2 = 0;
+				if (num2 >= 10) {
+					ten_num2 = num2 / 10; // 10의자리
+					one_num2 = num2 % 10; // 1의 자리
+				}
+				if (temp >= 10 && num2 >= 10) { // num1이 10보다 크고, num2도 10보다 크다면
+					if (alpha[temp / 10] > alpha[ten_num2]) { // 우선 순위가 크다면
+						temp = num2; // 값 갱신
+						idx = j;
+					} else if (temp / 10 == ten_num2 && alpha[temp % 10] > alpha[one_num2]) {// 10의 자리가 동일하다면 1의자리를 비교
+						temp = num2;
+						idx = j;
+					}
+				} else if (temp >= 10 && num2 < 10) { // num1이 10보다크고, num2가 10보다 작다면
+					if (alpha[temp / 10] > alpha[num2]) { // 우선 순위가 크다면
+						temp = num2;
+						idx = j;
+					} else if (temp / 10 == num2 && alpha[temp % 10] > alpha[num2]) {
+						temp = num2;
+						idx = j;
+					}
+				} else if (temp < 10 && num2 >= 10) { // num1이 10보다 작고, num2가 10보다 크다면
+					if (alpha[temp] > alpha[ten_num2]) { // 우선 순위가 크다면
+						temp = num2;
+						idx = j;
+					} else if (temp == ten_num2 && alpha[temp] > alpha[one_num2]) {
+						temp = num2;
+						idx = j;
+					}
+				} else {// 둘다 10보다 작다면
+					if (alpha[temp] > alpha[num2]) {
+						temp = num2;
+						idx = j;
+					}
+				}
+			}
+			list.add(temp); // 우선순위가 제일 작은 애가 list에 추가
+			arr.remove(idx); // list 빠진 해당 인덱스 제거
+		}
+		
+		int cnt = 0;
+		for (int i = 0; i < list.size(); i++) {
+			cnt++;
+			sb.append(list.get(i) + " ");
+			if (cnt % 10 == 0) {
+				sb.append("\n");
+				cnt = 0;
+			}
+		}
+		System.out.println(sb);
+	}
+}
