c0fdf54718785bc7ad4624882da303287895aa79
unknown <cbq3737@naver.com>
2021-09-27 23:10:05 +0900

    dp

---
+++ src/step14/num_23601.java
@@ -0,0 +1,64 @@
+package step14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class num_23601 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer tk = new StringTokenizer(br.readLine());
+		int nNum = Integer.parseInt(tk.nextToken()); //물품의 수
+		int mNum = Integer.parseInt(tk.nextToken());// 가방의 수
+		int[] weight = new int[nNum+1]; //무게 
+		int[] value = new int[nNum+1]; //가치
+		for(int i=1;i<=nNum;i++) {
+			tk = new StringTokenizer(br.readLine());
+			weight[i] = Integer.parseInt(tk.nextToken());
+			value[i] = Integer.parseInt(tk.nextToken());
+		}
+		int[] bag = new int[mNum+1];//가방
+		for(int i=1;i<=mNum;i++) {
+			tk = new StringTokenizer(br.readLine());
+			bag[i] = Integer.parseInt(tk.nextToken());
+		}
+		
+		double max =0;
+		int idx =0;
+		
+//		for(int i=1;i<=mNum;i++) { //2차원배열
+//			double[][] dp = new double[nNum+1][bag[i]+1]; //가방크기만큼 테이블 생성
+//			for(int r=1;r<=nNum;r++) {
+//				for(int c=1;c<=bag[i];c++) {
+//					if(weight[r]<=c) { //해당 물건을 가방에 담을 수 잇는지 						
+//						dp[r][c] = Math.max(dp[r-1][c], (dp[r-1][c-weight[r]]+value[r])); //담을 수 있다면 효율적인 것으로 넣자.
+//					}else {
+//						dp[r][c] = dp[r-1][c];
+//					}
+//				}
+//			}
+//			if(max<dp[nNum][bag[i]]/bag[i]) {
+//				max = dp[nNum][bag[i]]/bag[i];
+//				idx = i;
+//			}
+//		}
+		
+		for(int i=1;i<=mNum;i++) { //1차원배열
+			double[] dp = new double[bag[i]+1]; //가방크기만큼 테이블 생성
+			for(int r=1;r<=nNum;r++) {
+				for(int c=bag[i];c>=weight[r];c--) {
+					if(weight[r]<=c) { //해당 물건을 가방에 담을 수 잇는지 						
+						dp[c] = Math.max(dp[c], (dp[c-weight[r]]+value[r])); //담을 수 있다면 효율적인 것으로 넣자.
+					}
+				}
+			}
+			if(max<dp[bag[i]]/bag[i]) {
+				max = dp[bag[i]]/bag[i];
+				idx = i;
+			}
+		}
+
+		System.out.println(idx);
+	}
+}
--- src/step14/num_9084.java
+++ src/step14/num_9084.java
@@ -1,9 +1,30 @@
 package step14;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class num_9084 {
-	public static void main(String[] args) {
-	
-
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int tcase = Integer.parseInt(br.readLine());
+		for(int i=0;i<tcase;i++) {
+			int mNum = Integer.parseInt(br.readLine());
+			int[] arr = new int[mNum+1];
+			StringTokenizer tk= new StringTokenizer(br.readLine());
+			for(int t=1;t<=mNum;t++) {
+				arr[t] = Integer.parseInt(tk.nextToken());
+			}
+			int weight = Integer.parseInt(br.readLine());
+			int[] dp = new int[weight+1]; //1차원 오름차순으로 올라가면 중복 DP
+			dp[0] = 1;
+			for(int r=1;r<=mNum;r++) {
+				for(int c=arr[r];c<=weight;c++) {
+					dp[c] = dp[c]+dp[c-arr[r]];
+				}
+			}
+			System.out.println(dp[weight]);
+		}
 	}
-
 }
